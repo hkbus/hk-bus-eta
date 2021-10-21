fbad9b732bd453bcfff4f4244d1f7196d5a93c74
Law Wai Chun <chunlaw@rocketmail.com>
2021-10-21 20:27:14 +0800

    Bug fix on MTR bus

--- Lrtfeeder.js
+++ Lrtfeeder.js
@@ -17,12 +17,12 @@ module.exports = {
     .then(({busStop}) => 
       busStop.filter(({busStopId}) => busStopId === stopId)
       .reduce((ret, {bus: buses}) => ([...ret, ...buses.reduce( (etas, bus) => {
-        const etaDate = new Date(Date.now() + parseInt(bus.arrivalTimeInSecond, 10) * 1000)
-        if ( bus.arrivalTimeInSecond === "108000" ) return [...etas];
+        const etaDate = new Date(Date.now() + parseInt(bus.arrivalTimeInSecond === "108000" ? bus.departureTimeInSecond : bus.arrivalTimeInSecond, 10) * 1000);
         return [
           ...etas,
           {
-            eta: new Date(etaDate.toString().split('GMT')[0]+' UTC').toISOString(),
+            eta: `${etaDate.getFullYear()}-${`0${etaDate.getMonth() + 1}`.slice(-2)}-${`0${etaDate.getDate()}`.slice(-2)}`
+                +`T${`0${etaDate.getHours()}`.slice(-2)}:${`0${etaDate.getMinutes()}`.slice(-2)}:${`0${etaDate.getSeconds()}`.slice(-2)}+08:00`,
             remark: {
               [language]: bus.busRemark
             },
