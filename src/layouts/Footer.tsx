0d77b6930d0e1d5215fe8393413dd4d358bc831a
Law Wai Chun <chunlaw@rocketmail.com>
2023-09-13 13:48:38 +0800

    UI update

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,28 +1,7 @@
-import { GitHub as GitHubIcon } from "@mui/icons-material";
-import { Box, IconButton, SxProps, Theme } from "@mui/material";
-import npmLogo from "../assets/npm.svg";
+import { Box, SxProps, Theme } from "@mui/material";
 
 const Footer = () => {
-  return (
-    <Box sx={rootSx}>
-      <IconButton
-        onClick={() => {
-          window.open("https://github.com/hkbus/hk-bus-eta", "_blank");
-        }}
-        size="small"
-      >
-        <GitHubIcon />
-      </IconButton>
-      <IconButton
-        onClick={() => {
-          window.open("https://www.npmjs.com/package/hk-bus-eta", "_blank");
-        }}
-        size="small"
-      >
-        <img src={npmLogo} width={24} height={24} alt="NPM logo" />
-      </IconButton>
-    </Box>
-  );
+  return <Box sx={rootSx}>GPL-3.0 License © Chun Law</Box>;
 };
 
 export default Footer;
--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,9 +1,56 @@
-import { Box, SxProps, Theme, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  SxProps,
+  Theme,
+  Typography,
+} from "@mui/material";
+import { GitHub as GitHubIcon } from "@mui/icons-material";
+import pypiLogo from "../assets/pypi.svg";
+import npmLogo from "../assets/npm.svg";
 
 const Header = () => {
+
   return (
     <Box sx={rootSx}>
-      <Typography variant="h3">HK-BUS-ETA Demo</Typography>
+      <Typography variant="h6">
+        HK-BUS-ETA Demo
+      </Typography>
+      <Box>
+        <IconButton
+          onClick={() => {
+            window.open(
+              "https://github.com/hkbus/hk-bus-eta",
+              "_blank",
+            );
+          }}
+          size="small"
+        >
+          <GitHubIcon />
+        </IconButton>
+        <IconButton
+          onClick={() => {
+            window.open(
+              "https://www.npmjs.com/package/hk-bus-eta",
+              "_blank",
+            );
+          }}
+          size="small"
+        >
+          <img src={npmLogo} width={24} height={24} alt="NPM logo" />
+        </IconButton>
+        <IconButton
+          onClick={() => {
+            window.open(
+              "https://pypi.org/project/hk-bus-eta/",
+              "_blank",
+            );
+          }}
+          size="small"
+        >
+          <img src={pypiLogo} width={24} height={24} alt="Pypi logo" />
+        </IconButton>
+      </Box>
     </Box>
   );
 };
